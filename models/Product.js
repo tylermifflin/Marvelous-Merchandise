cf64ebdfd8647e46f03751001c735ca1b85972c0
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-17 14:00:09 -0600

    set up product.init function for id to have integer, allownull false primary key true autoincrement true

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,11 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+  id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
   },
   {
     sequelize,
