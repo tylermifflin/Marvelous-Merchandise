1bf01752dbd82cee5667f0444a8a65492f49ac9d
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-17 15:06:11 -0600

    set up the Product belongs to many tags through product tag and foreignkey product id

--- models/index.js
+++ models/index.js
@@ -16,6 +16,10 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
 
