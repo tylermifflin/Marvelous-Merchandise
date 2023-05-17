2d80f84b606f2d67eeb9b403be18422ac167b822
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-17 15:22:21 -0600

    set up router to get all products using the product.findall function and include category model tag through product tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({ 
+  // include its associated Category and Tag data
+  include: [
+    Category,
+     {
+        model: Tag,
+        through: ProductTag,
+     }
+  ]
+  })
+
 });
 
 // get one product
