ff80a8b59d99de38cfb8b50b94457b26659f6d1f
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-18 13:44:19 -0600

    used tag.destroy for delete function with same process

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -72,6 +72,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+  where: {
+    id: req.params.id,
+  },
+})
+  .then((tags) => res.json(tags))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  );
 });
 
 module.exports = router;
