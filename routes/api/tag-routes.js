cccbc410be57f14711db21da51e7567bcb66c83d
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-18 13:33:06 -0600

    setting up the router.get id to find single tag using tag.findone where and include

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,7 +24,17 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+  where: {
+    id: req.params.id,
+  },
   // be sure to include its associated Product data
+  include: [
+    {
+      model: Product,
+      through: ProductTag,
+    },
+  ],
 });
 
 router.post('/', (req, res) => {
