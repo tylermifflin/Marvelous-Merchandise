9246add7f07feeb4d072e899675a44b7b20179c9
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-18 13:59:10 -0600

    used Category.destroy for the delete function using where to delete by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,10 +43,24 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {where: {id: req.params.id}})
+  .then((categories) => res.json(categories))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  );
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({where: {id: req.params.id}})
+  .then((categories) => res.json(categories))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  );
 });
 
 module.exports = router;
